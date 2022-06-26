e50852260bef05bcb7271648c94b53e981617d27
John Paul Lim Gabule <lucas.gabule@gmail.com>
2022-06-27 02:31:13 +0800

    learn reactjs with typescript and redux

---
+++ src/state/reducers/repositoriesReducer.ts
@@ -0,0 +1,41 @@
+import { ActionType } from "../action-types";
+import { Action } from "../actions";
+
+interface IRepositoriesState {
+  loading: boolean;
+  error: string | null;
+  data: string[]
+}
+
+const initialState = {
+  loading: false,
+  error: null,
+  data: []
+}
+
+const reducer = (state: IRepositoriesState = initialState, action: Action): IRepositoriesState => {
+  switch (action.type) {
+    case ActionType.SEARCH_REPOSITORIES:
+      return {
+        loading: true,
+        error: null,
+        data: []
+      }
+    case ActionType.SEARCH_REPOSITORIES_SUCCESS:
+      return {
+        loading: false,
+        error: null,
+        data: action.payload
+      }
+    case ActionType.SEARCH_REPOSITORIES_ERROR:
+      return {
+        loading: false,
+        error: action.payload,
+        data: []
+      }
+    default:
+      return state;
+  }
+}
+
+export default reducer;
